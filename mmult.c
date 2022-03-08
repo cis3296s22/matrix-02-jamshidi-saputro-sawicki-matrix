1e251932a3b88062d39c6837d9e645983f2342a6
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-03-08 13:01:53 -0500

    Forgot to upload vectorized function

--- mmult.c
+++ mmult.c
@@ -2,15 +2,15 @@
  * An unoptimized implementation of matrix multiplication.
  */
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #include "mat.h"
 
 /**
  * An unoptimized algorithm for matrix multiplication.
- * 
+ *
  * @param c : the matrix in which to place the result of the matrix multiplication.
  * @param a : the first matrix.
  * @param aRows : the number of rows in a.
@@ -20,54 +20,30 @@
  * @param bCols : the number of columns in b.
  * @return 0 if the matrix multiplication is successful.
  */
-int mmult(double *c, 
-	      double *a, int aRows, int aCols, 
-	      double *b, int bRows, int bCols) {
+int mmult(double *c,
+		  double *a, int aRows, int aCols,
+		  double *b, int bRows, int bCols) {
+	// ORIGINAL GIVEN CODE (Non-vectorized)
+	for (int i = 0; i < aRows; i++) {
+		for (int j = 0; j < bCols; j++) {
+			c[i * bCols + j] = 0;
+			for (int k = 0; k < aRows; k++) {
+				c[i * bCols + j] += a[i * aRows + k] * b[k * bCols + j];
+			}
+		}
+	}
 
-    // ORIGINAL GIVEN CODE
-//    for(int i = 0; i < aRows; i++) {
-//        for(int j = 0; j < bCols; j++) {
-//            c[i * bCols + j] = 0;
-//            for(int k = 0; k < aRows; k++) {
-//                c[i * bCols + j] += a[i * aRows + k] * b[k * bCols + j];
-//            }
-//        }
-//    }
+	// THE FASTER WAY FOR MODERN CPU's SIMD (Vectorized)
+	// int N = aRows;
 
-    // OLDER SIMD
-//    for(int i = 0; i < aRows; i++) {
-//        for(int j = 0; j < bCols; j+=4) {
-//            c[i * bCols + j] = 0;
-//            for(int k = 0; k < aRows; k++) {
-//                c[i * bCols + (j + 0)] += a[i * aRows + k] * b[k * bCols + (j + 0)];
-//                c[i * bCols + (j + 1)] += a[i * aRows + k] * b[k * bCols + (j + 1)];
-//                c[i * bCols + (j + 2)] += a[i * aRows + k] * b[k * bCols + (j + 2)];
-//                c[i * bCols + (j + 3)] += a[i * aRows + k] * b[k * bCols + (j + 3)];
-//            }
-//        }
-//    }
+	// for (int i = 0; i < N; i++) {
+	// 	for (int j = 0; j < N; j++)
+	// 		c[i * bCols + j] = 0;
 
-    // THE FASTER WAY FOR MODERN CPU's SIMD
-    for (int i = 0; i < aRows; i++){
-        for (int j = 0; j < bCols; j++){
-            c[i * bCols + j] = 0;
-        }
+	// 	for (int k = 0; k < N; k++)
+	// 		for (int l = 0; l < N; l++)
+	// 			c[i * N + l] += a[i * N + k] * b[k * N + l];
+	// }
 
-        for (int k = 0; k < bCols; k++){
-            for (int l = 0; l < aRows; l+=4){
-                // c[i][l] = a[i][k] * b[k][l]
-                c[(i * bCols) + (l + 0)] += a[(i * aRows) + k] * b[(k * bCols) + (l + 0)];
-                c[(i * bCols) + (l + 1)] += a[(i * aRows) + k] * b[(k * bCols) + (l + 1)];
-                c[(i * bCols) + (l + 2)] += a[(i * aRows) + k] * b[(k * bCols) + (l + 2)];
-                c[(i * bCols) + (l + 3)] += a[(i * aRows) + k] * b[(k * bCols) + (l + 3)];
-            }
-        }
-    }
-
-
-
-  return 0;
+	return 0;
 }
-
-
-
