92ed5e46debe6e3b49bd6af30bb03e50b6abb1a5
Ayser Jamshidi <AceJamshidi4th@gmail.com>
2022-03-11 20:20:40 -0500

    mmult OMP implementation & code changes -Added mmult using OMP -Moved mmult OMP function documentation to mat.h -Cleaned up mmult functions

--- mmult.c
+++ mmult.c
@@ -9,12 +9,14 @@
 #include "mat.h"
 
 int mmult_vectorized(double *c, double *a, double *b, int N) {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++)
+	int i, j, k, l;
+
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++)
 			c[i * N + j] = 0;
 
-		for (int k = 0; k < N; k++)
-			for (int l = 0; l < N; l++)
+		for (k = 0; k < N; k++)
+			for (l = 0; l < N; l++)
 				c[i * N + l] += a[i * N + k] * b[k * N + l];
 	}
 
@@ -24,11 +26,13 @@ int mmult_vectorized(double *c, double *a, double *b, int N) {
 int mmult_nonvectorized(double *c,
 						double *a, int aRows, int aCols,
 						double *b, int bRows, int bCols) {
-	for (int i = 0; i < aRows; i++)
-		for (int j = 0; j < bCols; j++) {
+	int i, j, k;
+
+	for (i = 0; i < aRows; i++)
+		for (j = 0; j < bCols; j++) {
 			c[i * bCols + j] = 0;
 
-			for (int k = 0; k < aRows; k++)
+			for (k = 0; k < aRows; k++)
 				c[i * bCols + j] += a[i * aRows + k] * b[k * bCols + j];
 		}
 
